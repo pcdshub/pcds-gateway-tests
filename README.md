41f3cfea781348450afa115f6f67630a094513be
Ken Lauer <klauer@slac.stanford.edu>
2021-08-24 11:28:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+pcds-gateway-tests
+------------------
+
+Collection of tools to perform PCDS-specific gateway testing.
---
+++ access_rights.py
@@ -0,0 +1,79 @@
+import socket
+import sys
+from typing import Optional
+
+import caproto
+import caproto.sync.client as client
+
+GETHOSTNAME = socket.gethostname
+
+
+def check_access_security(
+    host_name: str,
+    pv_name: str,
+    timeout: float = client.common.GLOBAL_DEFAULT_TIMEOUT,
+    priority: int = 0,
+    udp_sock: Optional[socket.socket] = None,
+) -> caproto.AccessRightsResponse:
+    """
+    Read a Channel's access security settings for the given hostname.
+
+    Not thread-safe.
+
+    Parameters
+    ----------
+    host_name : str
+        The host name to check.
+    pv_name : str
+        The PV name to check.
+    timeout : float, optional
+        Default is 1 second.
+    priority : 0, optional
+        Virtual Circuit priority. Default is 0, lowest. Highest is 99.
+
+    Returns
+    -------
+    response : AccessRightsResponse
+    """
+
+    def get_host_name():
+        return host_name
+
+    close_udp = udp_sock is None
+    if udp_sock is None:
+        udp_sock = caproto.bcast_socket()
+        udp_sock.bind(("", 0))
+    try:
+        socket.gethostname = get_host_name
+        udp_sock.settimeout(timeout)
+        chan = client.make_channel(pv_name, udp_sock, priority, timeout)
+    finally:
+        socket.gethostname = GETHOSTNAME
+        if close_udp:
+            udp_sock.close()
+
+    try:
+        return chan.access_rights
+    finally:
+        try:
+            if chan.states[caproto.CLIENT] is caproto.CONNECTED:
+                client.send(chan.circuit, chan.clear(), chan.name)
+        finally:
+            client.sockets[chan.circuit].close()
+            del client.sockets[chan.circuit]
+            del client.global_circuits[(chan.circuit.address, chan.circuit.priority)]
+
+
+host_name, *pvnames = sys.argv[1:]
+# caproto.config_caproto_logging(level="DEBUG")
+
+udp_sock = caproto.bcast_socket()
+udp_sock.bind(("", 0))
+try:
+    for pvname in pvnames:
+        try:
+            print(pvname, check_access_security(host_name, pvname, udp_sock=udp_sock))
+        except TimeoutError:
+            print(pvname, "timeout")
+finally:
+    udp_sock.close()
